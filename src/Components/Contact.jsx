2faa4e05fc56675b7b6e5dffba8222ae99fc4254
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-05 19:32:02 +0530

    implemented contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { BsFillTelephoneFill, BsGithub } from "react-icons/bs";
 import { MdAttachEmail } from "react-icons/md";
 import { ImLinkedin } from "react-icons/im";
 import { FaHome } from "react-icons/fa";
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  SimpleGrid,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+import emailjs from "emailjs-com";
 
+const initialData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  message: "",
+};
 const Contact = () => {
+  const [formData, setFormData] = useState(initialData);
+  const [status, setStatus] = useState("")
+
+  const form = useRef();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+    const emailData = {
+      from_name: formData.firstName + " " + formData.lastName,
+      email: formData.email,
+      message: "Email: " + formData.email + " \n" + formData.message,
+    };
+    try {
+      await emailjs.send(
+        "service_xz0chow", // Replace with your Email.js service ID
+        "template_oh8vj5j", // Replace with your Email.js template ID
+        emailData,
+        "vu3_2UBLw2XV8K8XI"
+      );
+
+      // Display a success message to the user or redirect them to a confirmation page
+      console.log("Email sent successfully");
+      setStatus("Email sent successfully")
+    } catch (error) {
+      console.error("Error sending email:", error);
+      setStatus("Internal error, Please try again later")
+    }
+    setFormData(initialData);
+  };
+
   return (
     <Box
       id="contact"
@@ -121,6 +173,65 @@ const Contact = () => {
                 <Text as="b">LinkedIn</Text>
               </Box>
             </a>
+          </Box>
+          <Box
+            fontSize="20px"
+            justifyContent="left"
+            alignItems="center"
+            gap="6px"
+          >
+            <Text as="b">Get in Touch with Me</Text>
+
+            <Box w={"70%"}>
+              <form ref={form} onSubmit={handleSubmit}>
+                <FormControl>
+                  <Box display="flex" gap={"10px"}>
+                    <Input
+                      type="text"
+                      placeholder="First Name"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      required
+                    />
+                    <Input
+                      placeholder="Last Name"
+                      type="text"
+                      name="lastName"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      required
+                    />
+                  </Box>
+
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    m={"10px 0 10px 0"}
+                  />
+
+                  <Textarea
+                    type="text"
+                    placeholder="Message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                  />
+                  <Button type="submit" width={"100%"} m={"auto"} mt={"10px"}>
+                    Submit
+                  </Button>
+                </FormControl>
+              </form>
+
+              {
+                status && <Text color={status==="Email sent successfully" ? "green.800": "red.500"}>{status}</Text>
+              }
+              
+            </Box>
           </Box>
         </SimpleGrid>
       </Box>
